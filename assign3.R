483bf27809705359c4386788fb694b272d78fc37
Robin ROUZAUD <rouzaud.robin@orange.fr>
2017-10-07 13:23:29 +0200

    Min determination

--- assign3.R
+++ assign3.R
@@ -6,15 +6,25 @@ best <- function(state, outcome){
       close(con)
       
       # Are the inputs valid?
-      if(sum(as.character(state) == unique(data[, 7])) > 0){
-            print("State input valid")
-      }else{
+      if(sum(as.character(state) == unique(data[, 7])) == 0){
             return("Invalid state")
       }
       outcomeNames <- c("Heart Attack", "Heart Failure", "Pneumonia")
-      if(sum(outcome == outcomeNames) > 0){
-            print("Outcome input valid")
-      }else{
-            print("Invalid outcome")
+      if(sum(outcome == outcomeNames) == 0){
+            return("Invalid outcome")
       }
+      
+      # Subsetting data frame according to state input
+      stateDataFrame <- data[as.character(data[,7]) == state, ]
+      
+      # Calculating the minimum according to outcome input
+      outcomeCol <- c(11, 17, 23) # Matches outcomeNames
+      names(outcomeCol) <- outcomeNames
+      stateOutcome <- stateDataFrame[, outcomeCol[outcome]]
+      stateOutcome <- as.numeric(levels(stateOutcome))[stateOutcome]
+      min(stateOutcome, na.rm = TRUE)
 }
+
+
+
+
